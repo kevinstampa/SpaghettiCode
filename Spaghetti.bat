b0c0a61b1baf9f3e4d6bbcc5787c5ad41f9bdb16
kevinstampa <58338276+kevinstampa@users.noreply.github.com>
2019-11-29 17:38:41 +0100

    Create Spaghetti.bat

---
+++ Spaghetti.bat
@@ -0,0 +1,16 @@
+@echo off
+echo SpaghettiShell lets count!
+echo ...
+GOTO :A1
+:A2
+	echo SpaghettiShell/ 3
+	GOTO :A3
+ 
+:A1	
+	echo SpaghettiShell/ 1
+	GOTO :A2
+:A3 
+	echo SpaghettiShell/ 16726!
+echo ...
+	echo * SpaghettiShell has stopped working*
+	pause
